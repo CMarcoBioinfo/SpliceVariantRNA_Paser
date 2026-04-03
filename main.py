e76a9038dcea1d10553c6420b493e93da3927564
CMarcoBioinfo <corentin.marco.bioinfo@gmail.com>
2026-04-03 14:21:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -232,6 +232,7 @@ def main():
                 run_dir = os.path.dirname(values["-RUN-"])
                 run_base = os.path.basename(values["-RUN-"]).replace("_recap.zip", "")
                 qc_zip = os.path.join(run_dir, f"{run_base}_qc.zip")
+                print(qc_zip)
                 open_html_from_zip(qc_zip, "fastq_raw/", window, "FASTQ Raw QC")
 
 
@@ -239,12 +240,14 @@ def main():
                 run_dir = os.path.dirname(values["-RUN-"])
                 run_base = os.path.basename(values["-RUN-"]).replace("_recap.zip", "")
                 qc_zip = os.path.join(run_dir, f"{run_base}_qc.zip")
+                print(qc_zip)
                 open_html_from_zip(qc_zip, "fastq_trimmed/", window, "FASTQ Trimmed QC")
 
             if event == "-QC-BAM-":
                 run_dir = os.path.dirname(values["-RUN-"])
                 run_base = os.path.basename(values["-RUN-"]).replace("_recap.zip", "")
                 qc_zip = os.path.join(run_dir, f"{run_base}_qc.zip")
+                print(qc_zip)
                 open_html_from_zip(qc_zip, "BAM/", window, "BAM QC")
 
         
